a843c2841a4954cecf56b7cdebf5d0ce8cff0a2c
Hicode-dev <kingdavies2021@gmail.com>
2024-01-22 18:23:58 +0000

    correction:fixed the  graph  doughnut !

--- components/ui/graph/Creator-Doughnut.tsx
+++ components/ui/graph/Creator-Doughnut.tsx
@@ -1,40 +1,169 @@
+// import React from "react";
+// import { PieChart, Pie, Legend, Cell } from "recharts";
+
+// const data = [
+//   { name: "E-books 10.5k", value: 400, fill: "#04042A" },
+//   { name: "Services 5k", value: 300, fill: "#2063F2" },
+//   { name: "Art items 9k", value: 300, fill: "#FF73AE" },
+// ];
+
+// const renderColorfulLegendText = (value: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined, entry: any) => {
+//   return (
+//     <span style={{ color: 'white', fontWeight: 500, }}>
+//       {value}
+//     </span>
+//   );
+// };
+
+// const Doughnutt = () => {
+//   const isMobile = window.innerWidth <= 768;
+
+//   return (
+//     <div >
+//       <PieChart className="min-h-96 min-w-60">
+//       <Legend
+//           height={isMobile ? 60 : 120}
+//           iconType="circle"
+//           layout={isMobile ? "horizontal" : "vertical"}
+//           verticalAlign="middle"
+//           align="right"
+//           iconSize={isMobile ? 10 : 20}
+//           formatter={renderColorfulLegendText}
+//         />
+//         <Pie
+//           data={data}
+//           cx={isMobile ? 150 : 130}
+//           cy={200}
+//           innerRadius={isMobile ? 60 : 80}
+//           outerRadius={isMobile ? 120 : 130}
+//           fill="#8884d8"
+//           paddingAngle={0}
+//           dataKey="value"
+//         >
+ 
+//         </Pie>
+ 
+//       </PieChart>
+//     </div>
+//   );
+// };
+
+// export default Doughnutt;
+
+
 import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import {
+  PieChart,
+  Pie,
+  Legend,
+  Cell,
+  ResponsiveContainer,
+  Label,
+} from "recharts";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+const data01 = [
+  { name: "Art 10.5k", value: 35 },
+  { name: "E-books 5k", value: 25 },
+  { name: "Photography 9k", value: 30 },
+];
 
-export const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
+const COLORS = [" #2063F2", "#04042A ", '#FF73AE'];
+
+const Bullet = ({ backgroundColor, size }:any) => {
+  return (
+    <div
+      className="CirecleBullet rounded-full"
+      style={{
+        backgroundColor,
+        width: size,
+        height: size,
+      }}
+    ></div>
+  );
 };
 
-export function Doughnutt() {
+const CustomizedLegend = (props: { payload: any; }) => {
+  const { payload } = props;
   return (
-    <div className="">
-      <Doughnut data={data} />
+    <ul className="LegendList flex flex-col gap-3  absolute right-0 bottom-24" >
+      {payload.map((entry: { payload: { fill: any; value: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }; value: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }, index: any) => (
+        <li key={`item-${index}`} className="flex items-center">
+          <div className="BulletLabel  flex items-center gap-2">
+            <Bullet backgroundColor={entry.payload.fill} size="26px" />
+            <div className="BulletLabelText">{entry.value}</div>
+          </div>
+          {/* <div style={{ marginLeft: "10px" }}>{entry.payload.value}</div> */}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const CustomLabel = ({ viewBox, labelText, value }:any) => {
+  const { cx, cy } = viewBox;
+  return (
+    <g>
+      <text
+        x={cx}
+        y={cy}
+        className=""
+        textAnchor="middle"
+        dominantBaseline="central"
+        alignmentBaseline="middle"
+        fontSize="15"
+      >
+        {labelText}
+      </text>
+      <text
+        x={cx}
+        y={cy + 3}
+        className="recharts-text recharts-label text-white"
+        textAnchor="middle"
+        dominantBaseline="central"
+        alignmentBaseline="middle"
+        fill="white"
+        fontSize="26"
+        fontWeight="600"
+      >
+        {value}
+      </text>
+    </g>
+  );
+};
+
+const Doughnutt = () => {
+  return (
+    <div className="w-full min-h-96">
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={data01}
+            dataKey="value"
+            cx={160}
+            cy={250}
+            innerRadius={60}
+            outerRadius={130}
+          >
+            {data01.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+            <Label
+              content={<CustomLabel labelText="" value={'30.5k'} viewBox={undefined} />}
+              position="center"
+            />
+          </Pie>
+          <Legend content={<CustomizedLegend payload={undefined} />} />
+        </PieChart>
+      </ResponsiveContainer>
     </div>
   );
-}
+};
+
+export default Doughnutt;
+
+
+
+
