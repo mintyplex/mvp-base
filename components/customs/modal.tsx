bc56a81ef0c97d22cc30ef4b1ace80ab68cced01
BrightTechnologies1 <silascyrax@gmail.com>
2024-01-30 13:42:01 +0100

    update

--- components/customs/modal.tsx
+++ components/customs/modal.tsx
@@ -13,7 +13,7 @@ export default function EditModal({ setEditModal }: ModalProps) {
     return (
         <div className="bg-bg-dark/[0.65] fixed bottom-0 left-0 overflow-auto right-0 top-0 z-[9999] inset-0 backdrop-blur-[10px] px-4">
             <div className='relative flex items-center justify-center w-full h-screen'>
-                <div className='bg-[#313233] rounded-[8px] max-w-[600px] px-4 md:px-8 py-6 md:py-10'>
+                <div className='bg-[#313233] rounded-[8px] !max-w-[800px] px-4 md:px-8 py-6 md:py-10'>
                     <div className='w-full flex flex-col items-center justify-center gap-2'>
                         <div className='cursor-pointer w-full flex justify-end' onClick={() => setEditModal(false)}>
                             <AiOutlineCloseCircle size={30} />
@@ -34,9 +34,9 @@ export default function EditModal({ setEditModal }: ModalProps) {
                                 <textarea
                                     name=""
                                     placeholder="Provide a well detailed description of the item."
-                                    className="p-4 rounded-lg border-2 border-[rgb(99,99,99)] placeholder:text-[14px] outline-none bg-[rgb(44,45,46)] w-full "
+                                    className="p-4 rounded-lg border-2 border-[rgb(99,99,99)] text-[13px] placeholder:text-[14px] outline-none bg-[rgb(44,45,46)] w-full "
                                     id=""
-                                    rows={6}
+                                    rows={4}
                                 ></textarea>
                                 <label htmlFor="" className="px-4 text-sm">
                                     Bio
@@ -45,7 +45,7 @@ export default function EditModal({ setEditModal }: ModalProps) {
                             <div className="form">
                                 <input
                                     type="text"
-                                    className="p-4 border-2 border-[rgb(99,99,99)] placeholder:text-[14px] "
+                                    className="px-4 py-6 border-2 border-[rgb(99,99,99)] !text-[13px] placeholder:text-[14px] "
                                     placeholder="https://www.x.com/username"
                                     required
                                 />
