5bd02561fc49bb7420935cc6276d6bb952bc0906
EsinShadrach <esinshadrachrafe@gmail.com>
2024-02-03 22:28:48 +0100

    Completed ProductDetailed page

---
+++ app/[productId]/_components/counter.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { MinusIcon, PlusIcon } from "lucide-react";
+import React from "react";
+import { Button } from "~/components/ui/button";
+
+export function Counter() {
+  const [counter, setCounter] = React.useState(1);
+
+  return (
+    <div className="pt-3">
+      <div className="relative flex items-center justify-between p-3 border rounded-md border-mintyplex-border gap-3">
+        <div className="absolute -top-3.5 w-fit bg-mintyplex-dark">
+          <small>Quantity</small>
+        </div>
+        <div>{counter}</div>
+        <div className="space-x-3">
+          <Button
+            className="text-white bg-[#9F9F9F] hover:bg-[#9F9F9F] duration-300 transition-all"
+            onClick={() => setCounter((prev) => (prev > 0 ? prev - 1 : 0))}
+            size="icon"
+          >
+            <MinusIcon />
+          </Button>
+          <Button
+            onClick={() => setCounter((prev) => prev + 1)}
+            className="text-white duration-300 transition-all bg-mintyplex-primary"
+            size="icon"
+          >
+            <PlusIcon />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/[productId]/page.tsx
+++ app/[productId]/page.tsx
@@ -16,6 +16,32 @@ import {
 } from "~/components/ui/carousel";
 import productImg from "~/public/top-creator.jpeg";
 import BackButton from "../popular-products/_components/back-button";
+import { TypographyH2, TypographyH4, TypographyP } from "~/utils/typography";
+import { Badge } from "~/components/ui/badge";
+import { Counter } from "./_components/counter";
+
+const data = [
+  {
+    name: "DNA",
+    title: "Human",
+  },
+  {
+    name: "Eye Colour",
+    title: "Blue",
+  },
+  {
+    name: "Jewelry",
+    title: "Blue",
+  },
+  {
+    name: "Type",
+    title: "Blue",
+  },
+  {
+    name: "Clothing",
+    title: "Blue",
+  },
+];
 
 export default function ProductDetailPage() {
   return (
@@ -63,6 +89,71 @@ export default function ProductDetailPage() {
             </CarouselItem>
           </CarouselContent>
         </Carousel>
+      </div>
+      <div>
+        <TypographyH2 className="border-none">Yatch Ape Club</TypographyH2>
+        <div className="flex items-center pt-3 gap-1">
+          <span>by</span>
+          <Image
+            alt="By Creator"
+            src={productImg}
+            height={24}
+            width={24}
+            className="rounded-full"
+          />
+          <span className="underline">0x20...82</span>
+        </div>
+      </div>
+      <div className="">
+        <span className="leading-7">
+          The Bored Ape Yacht Club is a collection of 10,000 unique Bored Ape
+          NFTs— unique digital collectibles living on the Ethereum blockchain.
+          Your Bored Ape doubles as your Yacht Club membership card, and grants
+          access to members-only benefits, the first of which is access to THE
+          BATHROOM, a collaborative graffiti board. Future areas and perks can
+          be unlocked by the community through roadmap activation.
+        </span>
+        <Button
+          className="ml-2 transition-all duration-300"
+          variant={"secondary"}
+          size="sm"
+        >
+          See More
+        </Button>
+      </div>
+      <div>
+        <span className="mr-2 underline">Tags:</span>
+        {["NFT", "Art", "Collectibles", "Crypto"].map((tag) => (
+          <Badge
+            key={tag}
+            className="mr-2 text-white rounded-none bg-mintyplex-primary"
+          >
+            {tag}
+          </Badge>
+        ))}
+      </div>
+      <div className="border rounded-md divide-y border-mintyplex-border divide-mintyplex-border">
+        {data.map((item) => (
+          <div
+            key={item.name}
+            className="flex items-center justify-between p-3"
+          >
+            <div>{item.name}</div>
+            <div>{item.title}</div>
+          </div>
+        ))}
+      </div>
+      <Counter />
+      <div className="flex items-center justify-between">
+        <div className="flex items-end gap-1">
+          <TypographyH4 className="text-transparent !bg-clip-text font-medium [background:linear-gradient(87.25deg,_#2063f2,_#a431ff_33.33%,_#a431ff_66.67%,_#ff73ae)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+            $ 23
+          </TypographyH4>
+          <div className="font-semibold">
+            <s>$ 29</s>
+          </div>
+        </div>
+        <Button className="text-white bg-mintyplex-primary">Buy Now</Button>
       </div>
     </section>
   );
