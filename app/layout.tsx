374f8ea2460d94f4f7b21990860e878549e3a79f
EsinShadrach <esinshadrachrafe@gmail.com>
2024-02-04 23:17:49 +0100

    Added og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,8 +31,40 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // Something is wrong with the open graph of the meta data
   return (
     <html lang="en" className="bg-mintyplex-dark">
+      <title>Mintyplex - NFT Marketplace</title>
+      <meta
+        name="description"
+        content="Discover, buy and sell NFTs on Mintyplex"
+      />
+
+      <meta property="og:url" content="https://testnet.mintyplex.com/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Mintyplex - NFT Marketplace" />
+      <meta
+        property="og:description"
+        content="Discover, buy and sell NFTs on Mintyplex"
+      />
+      <meta
+        property="og:image"
+        content="https://opengraph.b-cdn.net/production/documents/71547070-99da-47bc-b4fd-c0f513608ba7.jpg?token=U0NgindjiwUYjNnKV68zje0bTqT-gP51QbmGlBWCrHE&height=779&width=1200&expires=33243084907"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="testnet.mintyplex.com" />
+      <meta property="twitter:url" content="https://testnet.mintyplex.com/" />
+      <meta name="twitter:title" content="Mintyplex - NFT Marketplace" />
+      <meta
+        name="twitter:description"
+        content="Discover, buy and sell NFTs on Mintyplex"
+      />
+      <meta
+        name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/documents/71547070-99da-47bc-b4fd-c0f513608ba7.jpg?token=U0NgindjiwUYjNnKV68zje0bTqT-gP51QbmGlBWCrHE&height=779&width=1200&expires=33243084907"
+      />
+
       <body className={cn(inter.className, "bg-mintyplex-dark text-white")}>
         <BurntWrapper disable>
           <ThemeProvider
