0c891753967e22135351ff93d78f7fd68f840609
David Anyatonwu <davidanyatonwu@gmail.com>
2024-02-09 17:34:41 +0100

    fix: import

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,4 +1,4 @@
-import connectMongoDB from "@/libs/mongodb";
+import connectMongoDB from "~/lib/utils/utils";
 import User from "../../../models/userModel";
 import { NextResponse } from "next/server";
 
--- components/customs/burnt-wrapper.tsx
+++ components/customs/burnt-wrapper.tsx
@@ -8,10 +8,14 @@ export default function BurntWrapper({
 }: React.PropsWithChildren<BurntWrapperProps>) {
   const seatContractAddress =
     "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka";
+  const mintyplexContractAddress = "xion1h6hrq6wlfrt53aafpt27uvvrmwqeqwrcuqdy4uzcdn4jqz2sqdcq8egzfu";
 
   return (
-    <AbstraxionProvider config={{ contracts: [seatContractAddress] }}>
+    <AbstraxionProvider config={{ contracts: [seatContractAddress, mintyplexContractAddress] }}>
       {children}
     </AbstraxionProvider>
   );
 }
+
+
+// xiond tx wasm store artifacts / mintyplex - contract.wasm--from mintyplex--gas - prices 0.1uxion--gas auto--gas - adjustment 1.3 - y--output json - b block
