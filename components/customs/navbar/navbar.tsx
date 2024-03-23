99c2d8cc26f553457453aaf5865f6efb0d59c13a
oxbryte <silascyrax@gmail.com>
2024-03-23 19:57:55 +0100

    update: profile drop down

--- components/customs/navbar/navbar.tsx
+++ components/customs/navbar/navbar.tsx
@@ -10,9 +10,18 @@ import MobileSidebar from "~/components/dashboardlayout/mobileSidebar";
 import Search from "~/components/ui/Search";
 import { Button } from "~/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { Input } from "~/components/ui/input";
 import logoLg from "~/public/logo-lg.png";
 import logo from "~/public/logo.png";
+import { truncate } from "~/utils/truncate";
 import { TypographyH3 } from "~/utils/typography";
 
 export default function Navbar() {
@@ -85,26 +94,37 @@ export default function Navbar() {
           <div className="hidden md:block">
             <div className="hidden md:block">
               {account.bech32Address ? (
-                <div onClick={() => setShowAbstraxion(true)}>
-                  {/* <DropdownMenu>
+                <div>
+                  <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                       <Button
                         variant="ghost"
-                        className="border border-mintyplex-border"
+                        className="w-fit px-4 py-5 gap-2 border border-mintyplex-border"
                         size="icon"
                       >
                         <HiUserCircle size={24} />
+                        {truncate(account.bech32Address)}
                       </Button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuContent className="mt-3 w-[144px] border-mintyplex-border bg-mintyplex-dark">
+                      <Link href="/dashboard">
+                        <DropdownMenuItem className="cursor-pointer">
+                          Dashboard
+                        </DropdownMenuItem>
+                      </Link>
                       <DropdownMenuSeparator />
-                      <DropdownMenuItem>Profile</DropdownMenuItem>
-                      <DropdownMenuItem>Billing</DropdownMenuItem>
-                      <DropdownMenuItem>Team</DropdownMenuItem>
-                      <DropdownMenuItem>Subscription</DropdownMenuItem>
+                      <DropdownMenuItem className="cursor-pointer">
+                        Profile
+                      </DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem
+                        className="cursor-pointer hover:bg-red"
+                        onClick={() => setShowAbstraxion(true)}
+                      >
+                        Logout
+                      </DropdownMenuItem>
                     </DropdownMenuContent>
-                  </DropdownMenu> */}
+                  </DropdownMenu>
                 </div>
               ) : (
                 <button
