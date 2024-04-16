e12d4c74137811bd46712c6d95b5fb500797bbbd
oxbryte <silascyrax@gmail.com>
2024-04-16 22:45:44 +0100

    update: user data update

--- components/customs/modal.tsx
+++ components/customs/modal.tsx
@@ -5,12 +5,25 @@ import Curator from '~/public/curator.png'
 import React, { useRef, useState } from "react";
 import { Button } from "../ui/button";
 import { FaCamera } from "react-icons/fa6";
+import usePutData from "~/hooks/usePutData";
+import { useAccount } from "../context/AccountContext";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 
 type ModalProps = {
   setEditModal?: any;
 };
 
+interface FormData {
+  bio: string;
+  email: string;
+  wallet_address: string;
+  x_link: string;
+}
+
 export default function EditModal({ setEditModal }: ModalProps) {
+
+  const { account } = useAccount();
 
   const [imageSrc, setImageSrc] = useState<any>(Curator);
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -28,6 +41,35 @@ export default function EditModal({ setEditModal }: ModalProps) {
 
   const triggerFileInput = () => {
     fileInputRef.current?.click();
+  };
+
+  // form handle
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { postData, isLoading, error } = usePutData();
+
+  const onSubmit = async (data: any) => {
+    // preventDefault();
+    const apiUrl = "https://mintyplex-api.onrender.com/api/v1/user";
+    // const apiUrl = process.env.NEXT_BASE_URL;
+
+    // data.wallet_address = account.bech32Address;
+
+    console.log(data);
+
+    const response = await postData({
+      url: `${apiUrl}/profile`,
+      body: data,
+    });
+    if (response) {
+      console.log("Form submitted successfully:", response);
+      setEditModal(false)
+    }
   };
 
   return (
@@ -72,37 +114,51 @@ export default function EditModal({ setEditModal }: ModalProps) {
                 ref={fileInputRef}
               />
             </div>
-            <form className="flex flex-col gap-3 w-full">
-              <div className="form">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3 w-full">
+            <div className="form">
+                <p className="mb-2 text-[14px]">
+                  Bio <span className="text-red-600">*</span>
+                </p>
                 <textarea
-                  name=""
+                  // name="bio"
                   placeholder="Provide a well detailed description of the item."
-                  className="p-4 rounded-lg border-2 border-[rgb(99,99,99)] text-[13px] placeholder:text-[14px] bg-none outline-none  w-full "
+                  className="p-4 rounded-lg border-2 border-[rgb(99,99,99)] text-[13px] placeholder:text-[14px] outline-none  w-full "
                   id=""
                   rows={4}
+                  {...register("bio", { required: true })}
                 ></textarea>
-                <label htmlFor="" className="px-4 text-sm">
-                  Bio
-                </label>
               </div>
               <div className="form">
+                <p className="mb-2 text-[14px]">
+                  X(Twitter) link <span className="text-red-600">*</span>
+                </p>
                 <input
-                  type="text"
-                  className="px-4 py-6 border-2 border-[rgb(99,99,99)] !text-[13px] placeholder:text-[14px] "
+                  type="url"
+                  // name="x_link"
+                  className="p-4 bg-none border-2 border-[rgb(99,99,99)] !text-[13px] placeholder:text-[14px] "
                   placeholder="https://www.x.com/username"
                   required
+                  {...register("x_link", { required: true })}
                 />
-                <label htmlFor="" className="px-4 text-sm">
-                  X(Twitter) link <span className="text-red-600">*</span>
-                </label>
               </div>
             </form>
             <div className="w-full flex justify-end mt-4">
-              <Button className="text-white bg-mintyplex-primary">
-                Update
-              </Button>
+              <button
+                onClick={handleSubmit(onSubmit)}
+                // disabled={isLoading}
+                className="text-white bg-mintyplex-primary px-3 py-2 rounded-[8px]"
+              >
+                {isLoading ? (
+                  <>
+                    <div className="loader"></div>
+                  </>
+                ) : (
+                  "Update"
+                )}
+              </button>
             </div>
           </div>
+          {error && <p>{error}</p>}
         </div>
       </div>
     </div>
