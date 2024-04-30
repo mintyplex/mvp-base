66026a7e2ed194731e932c7e2986c10d578fde37
EsinShadrach <esinshadrachrafe@gmail.com>
2024-05-01 00:11:21 +0100

    Remved env from next config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ["mintyplex-api.onrender.com", "localhost:3000"],
   },
-  env: {
-    NEXT_BASE_URL: process.env.NEXT_BASE_URL,
-  },
 };
 
 module.exports = nextConfig;
