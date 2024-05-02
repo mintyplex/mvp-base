0ac763d420ba26cc7bd1dd49b6c9b7fdae12ad02
oxbryte <silascyrax@gmail.com>
2024-05-02 21:29:07 +0100

    update: Smart contract

--- components/customs/burnt-wrapper.tsx
+++ components/customs/burnt-wrapper.tsx
@@ -6,9 +6,7 @@ type BurntWrapperProps = {};
 export default function BurntWrapper({
   children,
 }: React.PropsWithChildren<BurntWrapperProps>) {
-  const seatContractAddress =
-    "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka";
-  const mintyplexContractAddress = "xion1h6hrq6wlfrt53aafpt27uvvrmwqeqwrcuqdy4uzcdn4jqz2sqdcq8egzfu";
+  const mintyplexContractAddress = "xion1gdzk8u6z3u46220qtlr6kktqv023dsyh32j22c05vnpr5wnfulmsxvcwrv";
 
   return (
     <AbstraxionProvider config={{ contracts: [mintyplexContractAddress] }}>
