aa3597cf8de1af9e283fe4880b0f1d22f3e92d38
oxbryte <silascyrax@gmail.com>
2024-06-01 22:18:41 +0100

    update: error

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
   images: {
     domains: ["mintyplex-api.onrender.com", "localhost:3000", "as1.ftcdn.net"],
   },
-};
-const typescriptRemove = {
   typescript: {
     ignoreBuildErrors: true,
   },
 };
-module.exports = { nextConfig, typescriptRemove };
+module.exports = nextConfig;
